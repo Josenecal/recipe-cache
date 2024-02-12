d51e103a9f6d1267d741d21a1585b243df968055
Joseph Senecal <jasenecal2@gmail.com>
2024-02-12 15:16:35 -0700

    Remove unnecessary table_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-    self.table_name = 'users'
     has_secure_password
-
     validates_presence_of :first_name, :last_name, :email, :password, message: "First Name, Last Name, Email and Password are required fields"
 end
