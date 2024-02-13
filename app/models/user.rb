9fe5b13d4fbd35f414aa045e13f72e6ecc0d4fd8
Joseph Senecal <jasenecal2@gmail.com>
2024-02-13 12:46:23 -0700

    Add email uniqueness constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
     validates_presence_of :first_name, :last_name, :email, :password, message: "First Name, Last Name, Email and Password are required fields"
+    validates_uniqueness_of :email
 end
