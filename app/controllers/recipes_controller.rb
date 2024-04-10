b8c9118270e116a84884fe83a53b000d6f25442a
Joseph Senecal <jasenecal2@gmail.com>
2024-04-10 15:04:54 -0600

    Recipe create controller method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,23 @@ class RecipesController < ApplicationController
 
     def new
         @new_recipe = Recipe.new
+        @new_recipe.recipe_ingredients.new
     end
 
+    def create
+        @new_recipe = Recipe.new(new_recipe_params)
+        if @new_recipe.save
+            flash[:message] = "Recipe Created!"
+            redirect_to "/recipes"
+        else
+            flash[:error] = "Hmmm, something went wrong. Sorry about that! Pleast try again."
+            redirect_to "/recipes/new"
+        end
+    end
+
+    private
+
+    def new_recipe_params
+        params.require(:recipe).permit(:name, :description, recipe_ingredients_attributes: [:id, :name, :ammount, :preparation, :_destroy])
+    end
 end
