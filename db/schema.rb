a331cce38e73a48469262da7237a6a2035f2fcff
Joseph Senecal <jasenecal2@gmail.com>
2024-04-22 16:32:17 -0600

    Add private attribute to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_191925) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_195811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_22_191925) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.boolean "private", default: false
     t.index ["author_id"], name: "index_recipes_on_author_id"
   end
 
