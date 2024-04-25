22cc2a47ed29727ef7ef764b828b35cfde02a002
Joseph Senecal <jasenecal2@gmail.com>
2024-04-25 10:42:43 -0600

    Controller method for UserRecipe Delete

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -15,4 +15,14 @@ class UserRecipesController < ApplicationController
         end
     end
 
+    def destroy
+        if params[:user_id].to_i == current_user.id
+            user_recipe = UserRecipe.where(recipe_id: params[:recipe_id], user_id: params[:user_id]).destroy_all
+            flash[:message] = "Recipe removed from library."
+        else
+            flash[:error] = "Something went wrong, recipe was not removed"
+        end
+        redirect_back_or_to "/recipes"
+    end
+
 end
