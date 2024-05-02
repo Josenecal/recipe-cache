e39d07bc09f54d8bacb169c0286f2e7034f73f8f
Joseph Senecal <jasenecal2@gmail.com>
2024-05-02 08:58:53 -0600

    Add unit test for User#full_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe 'User' do
             expect(new_user.id).not_to be nil
         end
     end
+
+    describe 'Unit Tests' do
+        describe '#full_name' do
+
+            let(:new_user) {FactoryBot.create(:user)}
+
+            it "pulls the first_name and last_name from a user's record, concatinates and retusn them" do
+                expected = "#{new_user.first_name} #{new_user.last_name}"
+
+                expect(new_user.full_name).to match expected
+            end
+        end
+    end
 end
